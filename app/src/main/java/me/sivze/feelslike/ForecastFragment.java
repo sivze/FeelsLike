76fd293878e89b869a964caa31c27f9482c0f094
Sivakumar Chellamuthu <siva@go-text.me>
2016-11-06 23:37:10 -0800

    Added contextual empty view around network status.

--- app/src/main/java/me/sivze/feelslike/ForecastFragment.java
+++ app/src/main/java/me/sivze/feelslike/ForecastFragment.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import me.sivze.feelslike.data.WeatherContract;
 import me.sivze.feelslike.sync.SunshineSyncAdapter;
@@ -150,8 +151,8 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
         View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
         View emptyView = rootView.findViewById(R.id.listview_forecast_empty);
-        mListView.setEmptyView(emptyView);
         mListView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        mListView.setEmptyView(emptyView);
         mListView.setAdapter(mForecastAdapter);
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -259,6 +260,26 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
             // If we don't need to restart the loader, and there's a desired position to restore
             // to, do so now.
             mListView.smoothScrollToPosition(mPosition);
+        }
+        updateEmptyView();
+    }
+
+    /*
+            Updates the empty list view with contextually relevant information that the user can
+            use to determine why they aren't seeing weather.
+         */
+
+    private void updateEmptyView() {
+        if (mForecastAdapter.getCount() == 0) {
+            TextView tv = (TextView) getView().findViewById(R.id.listview_forecast_empty);
+            if (null != tv) {
+                // if cursor is empty, why? do we have an invalid location
+                int message = R.string.empty_forecast_list;
+                if (!Utility.isNetworkAvailable(getActivity())) {
+                    message = R.string.empty_forecast_list_no_network;
+                }
+                tv.setText(message);
+            }
         }
     }
 
