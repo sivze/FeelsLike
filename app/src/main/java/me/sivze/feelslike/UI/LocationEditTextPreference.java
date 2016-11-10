33a7afd2896c08b4801680ab4cf06c2c92d2431e
Sivakumar Chellamuthu <siva@go-text.me>
2016-11-09 19:55:53 -0800

    Implement location string limits part 1

---
+++ app/src/main/java/me/sivze/feelslike/UI/LocationEditTextPreference.java
@@ -0,0 +1,30 @@
+package me.sivze.feelslike.UI;
+
+/**
+ * Created by siva on 11/9/16.
+ */
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.EditTextPreference;
+import android.util.AttributeSet;
+
+import me.sivze.feelslike.R;
+
+public class LocationEditTextPreference extends EditTextPreference {
+    static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    private int mMinLength;
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.LocationEditTextPreference,
+                0, 0);
+        try {
+            mMinLength = a.getInteger(R.styleable.LocationEditTextPreference_minLength, DEFAULT_MINIMUM_LOCATION_LENGTH);
+        } finally {
+            a.recycle();
+        }
+    }
+}
