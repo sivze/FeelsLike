183df96cf4f230094a0f5ff9df9f3a0db26ad604
Sivakumar Chellamuthu <siva@go-text.me>
2016-11-09 20:03:35 -0800

    Implement location string limits part 2

--- app/src/main/java/me/sivze/feelslike/UI/LocationEditTextPreference.java
+++ app/src/main/java/me/sivze/feelslike/UI/LocationEditTextPreference.java
@@ -4,16 +4,57 @@ package me.sivze.feelslike.UI;
  * Created by siva on 11/9/16.
  */
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 import me.sivze.feelslike.R;
 
 public class LocationEditTextPreference extends EditTextPreference {
     static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
     private int mMinLength;
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+        EditText editText = getEditText();
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog d = getDialog();
+                if (d instanceof AlertDialog) {
+                    AlertDialog dialog = (AlertDialog) d;
+                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    // Check if the EditText is empty
+                    if (s.length() < mMinLength) {
+                        // Disable OK button
+                        positiveButton.setEnabled(false);
+                    } else {
+                        // Re-enable the button.
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
+    }
 
     public LocationEditTextPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
